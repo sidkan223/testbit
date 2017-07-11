df990842a695b5d17795bf46da631688c1cc3dff
Marcuss Slavenas <slavenas@illinois.edu>
2017-07-11 11:21:13 -0500

    Add convert function for converting geocode deg,min,sec to decimal

--- pyclowder/geostreams/geocode_convert.py
+++ pyclowder/geostreams/geocode_convert.py
@@ -2,7 +2,7 @@ from LatLon import LatLon
 from LatLon import Latitude
 from LatLon import Longitude
 
-"""Convert a gecode 2d list from [[deg,min,sec],[deg,min,sec]] to [decimal,decimal] """
+"""Convert a gecode 2d list from [[deg,min,sec],[deg,min,sec]] to [decimal,decimal] , both ordered - 'lat'long' """
 def dms2dec(dms):
     dec = list(LatLon(Latitude(degree = dms[0][0], minute = dms[0][1], second = dms[0][2]), Longitude(degree = dms[1][0], minute = dms[1][1], second = dms[1][2])).to_string())
 
