2efdb47143467c2a98277af6a38b046614f00eeb
Marcuss Slavenas <slavenas@illinois.edu>
2017-07-11 16:43:12 -0500

    comments

--- pyclowder/geostreams/time_transformers.py
+++ pyclowder/geostreams/time_transformers.py
@@ -3,13 +3,14 @@ from datetime import datetime,timedelta
 from pytz import timezone, utc
 
 
-# CONVERT TO UTC FORMAT            FROM EXAMPLE FORMAT 2015-11-20 03:46:20
+# convert to utc format from 2015-11-20 03:46:20
 def time2utc(date_in, time_zone="America/Chicago"):
     date_out = timezone(time_zone).localize(datetime.strptime(date_in, "%Y-%m-%d %H:%M:%S")).astimezone(utc).strftime(
         "%Y-%m-%dT%H:%M:%SZ")
 
     return date_out
 
+# convert year and julian day to date
 def julian_day_to_month_day(year,julian_day):
     date = datetime(int(year), 1, 1) + timedelta(int(julian_day))
 
