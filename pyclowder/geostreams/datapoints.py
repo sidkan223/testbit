cba892b38f14676e92f5cf3b4b0cc5b817758d37
Marcuss Slavenas <slavenas@illinois.edu>
2017-07-11 11:49:53 -0500

    change datapoint_create_json to input properties as dict instead of 2 lists

--- pyclowder/geostreams/datapoints.py
+++ pyclowder/geostreams/datapoints.py
@@ -71,8 +71,7 @@ class DatapointsApi(object):
 
         return latest_datapoint
 
-    def datapoint_create_json(self, start_time, end_time, longitude, latitude, sensor_id, stream_id, sensor_name,
-                              property_ids, properties, owner=None, source=None, procedures=None, elevation=0):
+    def datapoint_create_json(self, start_time, end_time, longitude, latitude, sensor_id, stream_id, sensor_name, properties, owner=None, source=None, procedures=None, elevation=0):
         datapoint = {
             'start_time': start_time,
             'end_time': end_time,
@@ -100,7 +99,7 @@ class DatapointsApi(object):
         if procedures is not None:
             datapoint['properties']['procedures'] = procedures
 
-        for i in range(len(property_ids)):
-            datapoint['properties'][property_ids[i]] = properties[i]
+
+        datapoint['properties'] = properties
 
         return datapoint
