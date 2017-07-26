076160c339570015dcf4466f3d492001a11390ec
Marcuss Slavenas <slavenas@illinois.edu>
2017-07-26 10:51:27 -0500

    Add calendar to standard format converter

--- pyclowder/geostreams/time_transformers.py
+++ pyclowder/geostreams/time_transformers.py
@@ -15,3 +15,10 @@ def julian_day_to_month_day(year,julian_day):
     date = datetime(int(year), 1, 1) + timedelta(int(julian_day))
 
     return date
+
+# convert calendar date month/day/2-digit-year to YYYY-MM-DDT00:00:00Z (assumes there is no time so all set to zero
+def calendar_date2utc(date_in,time_zone="America/Chicago"):
+
+    date_out = datetime.strptime(date_in + " 00:00:00", "%m/%d/%y %H:%M:%S").strftime("%Y-%m-%dT%H:%M:%SZ")
+
+    return date_out
